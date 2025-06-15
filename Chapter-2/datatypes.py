57e17ce5df7cfe51378dd3d6f4cb005c1f3d7aa3
Abdul Manan <abdul.manan232332@gmail.com>
2025-06-16 01:50:37 +0500

    learning of Chapter-2

---
+++ Chapter-2/datatypes.py
@@ -0,0 +1,11 @@
+a = 1 # integer Type
+b = 1.1 # float Type
+c = "Abdul Manan" # string Type
+d = True # boolean Type
+e = None # none Type
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
---
+++ Chapter-2/operators.py
@@ -0,0 +1,32 @@
+# Arithmetic Operators
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+
+# Assignment Operators
+a = 10
+b = 5
+a += b
+a -= b
+print(a)
+
+# Comparison Operators
+a = 10
+b = 5
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+# Logical Operators
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
