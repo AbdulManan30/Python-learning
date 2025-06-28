031e9b9c9b5b0fff364f4f637fb11520a3123fe6
Abdul Manan <abdul.manan232332@gmail.com>
2025-06-28 21:03:09 +0500

    2nd Project is Completed

--- 2nd-Project/main.py
+++ 2nd-Project/main.py
@@ -8,10 +8,9 @@ while i != n:  # loop until the user guesses the number
     i = int(input("Guess a number between 1 and 100: "))
     if n > i:
         print("Higher Number Please")
+        guesses += 1
+
     elif n < i:
         print("Lower Number Please")
-    guesses += 1
-
+        guesses += 1
 print("You guessed it in", guesses, "guesses", "The Number is ", n)
-
-
