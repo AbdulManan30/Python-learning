f24074c40660376d34ed2a8c780a9666815093a2
Abdul Manan <abdul.manan232332@gmail.com>
2025-06-28 20:58:55 +0500

    2nd Project is Completed

---
+++ 2nd-Project/main.py
@@ -0,0 +1,15 @@
+# Create a Guess Number Game
+import random
+
+n = random.randint(1, 100)
+i = 0
+guesses = 0
+while i != n:  # loop until the user guesses the number
+    i = int(input("Guess a number between 1 and 100: "))
+    if n > i:
+        print("Higher Number Please")
+    elif n < i:
+        print("Lower Number Please")
+    guesses += 1
+
+print("You guessed it in", guesses, "guesses", "The Number is ", n)
